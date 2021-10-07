656115c178b2be59d6efb9fd38b7d8f4663daa9b
Bag of Fleas <62733806+0xba60ff1ea5@users.noreply.github.com>
2021-10-06 21:16:53 -0500

    Add files via upload

---
+++ viewer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Very simple BASH script
+# Finds a random image in a directory and displays it for you
+
+echo "Running viewer.sh....."
+echo "Finding a random image in your preferred directory for you to view....."
+
+# Export the directory
+# Adjust this to your liking
+export IMAGES=/usr/my_garbage/images
+
+# Pick a random image in the dierectory
+myfile=$(ls $IMAGES | shuf -n 1)
+
+# Display the images
+eog $IMAGES/$myfile
+
+
+
+
