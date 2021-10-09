f19ddbc462778a294d7684e20b1d1e13ba89d790
Bag of Fleas <62733806+0xba60ff1ea5@users.noreply.github.com>
2021-10-08 19:09:39 -0500

    Update viewer.sh

--- viewer.sh
+++ viewer.sh
@@ -3,6 +3,10 @@
 # Very simple shell script
 # Finds a random image in a directory and displays it for you
 
+# Investigate source code
+# git clone https://github.com/GNOME/eog.git
+# How does it handle thumbnails?
+
 echo "Running viewer.sh....."
 echo "Finding a random image in your preferred directory for you to view....."
 
@@ -13,8 +17,10 @@ export IMAGES=/usr/my_garbage/images
 # Pick a random image in the directory
 myfile=$(ls $IMAGES | shuf -n 1)
 
-# Display the image
-eog $IMAGES/$myfile
+# Display the image along with any arguments passed in by the user
+# Article detailing how to use arguments
+# https://unix.stackexchange.com/questions/31414/how-can-i-pass-a-command-line-argument-into-a-shell-script
+eog $IMAGES/$myfile $1
 
 
 
